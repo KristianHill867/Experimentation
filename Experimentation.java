b1a4125d7525b535e1ba659999160fa66db60a9c
Kristian Hill <kristianbailey867@gmail.com>
2026-08-28 10:38:26 -0500

    Refactor Experimentation class and add date-time label
    Refactored Experimentation class to improve structure and added a date-time label feature.

--- Experimentation.java
+++ Experimentation.java
@@ -1,20 +1,31 @@
-import java.util.Scanner;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.io.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public class Experimentation
+public class Experimentation 
 {
-    public static class DescriptionFrame extends JFrame
+    private static JLabel createDateTimeLabel() 
     {
-        public DescriptionFrame(String description)
+        JLabel timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy  hh:mm:ss a");
+
+        timeLabel.setText(LocalDateTime.now().format(formatter));
+
+        Timer timer = new Timer(1000, e -> timeLabel.setText(LocalDateTime.now().format(formatter)));
+        timer.start();
+
+        return timeLabel;
+    }
+
+    public static class DescriptionFrame extends JFrame 
+    {
+        public DescriptionFrame(String description) 
         {
             super("Description");
-            setSize(300,150);
+            setSize(300, 150);
             setLocationRelativeTo(null);
             setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -24,25 +35,24 @@ public class Experimentation
             text.setWrapStyleWord(true);
 
             add(new JScrollPane(text), BorderLayout.CENTER);
-            setVisible(true);
         }
     }
 
-    class PasswordFrame extends JFrame
+    public static class PasswordDialog extends JDialog 
     {
         private boolean unlocked = false;
 
-        public boolean isUnlocked()
+        public boolean isUnlocked() 
         {
             return unlocked;
         }
 
-        public PasswordFrame()
+        public PasswordDialog() 
         {
-            super("Enter Code");
-            setSize(260,130);
+            super((Frame) null, "Enter Code", true); // modal dialog
+            setSize(260, 130);
             setLocationRelativeTo(null);
-            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
             setLayout(new FlowLayout());
 
             JLabel label = new JLabel("Enter 4-digit code: ");
@@ -55,12 +65,12 @@ public class Experimentation
             {
                 String code = new String(passField.getPassword());
 
-                if (code.equals("5150"))
+                if ("5150".equals(code)) 
                 {
                     unlocked = true;
                     dispose();
-                }
-                else
+                } 
+                else 
                 {
                     JOptionPane.showMessageDialog(this, "Incorrect code.");
                     passField.setText("");
@@ -73,109 +83,103 @@ public class Experimentation
             add(label);
             add(passField);
             add(submit);
-            setVisible(true);
         }
     }
 
-    public static void main (String[] args)
+    public static void main(String[] args) 
     {
-        Experimentation outer = new Experimentation();
-        PasswordFrame pf = outer.new PasswordFrame();
-
-        while (pf.isShowing())
+        SwingUtilities.invokeLater(() -> 
         {
-            try { Thread.sleep(100); } catch (Exception ex) {}
-        }
+            PasswordDialog dialog = new PasswordDialog();
+            dialog.setVisible(true);
 
-        if (!pf.isUnlocked()) System.exit(0);
+            if (!dialog.isUnlocked()) 
+            {
+                System.exit(0);
+            }
 
-        SimpleFrame frame = new SimpleFrame("File Selection Menu");
-        frame.setMinimumSize(new Dimension(300,200));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+            SimpleFrame frame = new SimpleFrame("File Selection Menu");
+            frame.setMinimumSize(new Dimension(300, 200));
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 
-    public static class SimpleFrame extends JFrame
+    public static class SimpleFrame extends JFrame 
     {
         private JLabel myLabel;
 
-        public SimpleFrame (String label)
+        public SimpleFrame(String label) 
         {
             super(label);
             setLayout(new BorderLayout());
 
-            JPanel mainPanel = new JPanel(new GridLayout(2,2,10,10));
+            JPanel headerPanel = new JPanel(new BorderLayout());
+            headerPanel.setOpaque(false);
+            JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            rightPanel.setOpaque(false);
+            rightPanel.add(createDateTimeLabel());
+            headerPanel.add(rightPanel, BorderLayout.EAST);
+            add(headerPanel, BorderLayout.NORTH);
+
+            JPanel mainPanel = new JPanel(new GridLayout(2, 2, 10, 10));
 
             Automobile acura = new Automobile(2010, "Acura", "MDX", "Blue", "123468");
             Automobile honda = new Automobile(2016, "Honda", "Civic", "Green", "65468484");
-            Automobile ford  = new Automobile(2004, "Ford",  "F150", "Black",  "351616484");
+            Automobile ford = new Automobile(2004, "Ford", "F150", "Black", "351616484");
             Automobile porsche = new Automobile(2005, "Porsche", "Boxter", "White", "44618445");
 
-            JButton button1 = new JButton("Blue");
-            JButton button2 = new JButton("Green");
-            JButton button3 = new JButton("Black");
-            JButton button4 = new JButton("White");
-
-            mainPanel.add(button1);
-            mainPanel.add(button2);
-            mainPanel.add(button3);
-            mainPanel.add(button4);
+            mainPanel.add(createCarButton("Blue", acura));
+            mainPanel.add(createCarButton("Green", honda));
+            mainPanel.add(createCarButton("Black", ford));
+            mainPanel.add(createCarButton("White", porsche));
 
             add(mainPanel, BorderLayout.CENTER);
 
             myLabel = new JLabel("Please Select A File", SwingConstants.CENTER);
             add(myLabel, BorderLayout.SOUTH);
 
-            ActionListener selectionListener = e -> myLabel.setText("Selection Process Complete");
-            button1.addActionListener(selectionListener);
-            button2.addActionListener(selectionListener);
-            button3.addActionListener(selectionListener);
-            button4.addActionListener(selectionListener);
-
-            button1.addActionListener(e -> showDescription(acura));
-            button2.addActionListener(e -> showDescription(honda));
-            button3.addActionListener(e -> showDescription(ford));
-            button4.addActionListener(e -> showDescription(porsche));
-
             new OtherFrame("Menu Bar");
 
             pack();
-            setVisible(true);
         }
 
-        private void showDescription(Object item)
+        private JButton createCarButton(String color, Automobile car) 
         {
-            String text;
-            if (item instanceof Automobile auto)
-            {
-                text = auto.getCustomDescription();
-            }
-            else
-            {
-                text = item.toString();
-            }
+            JButton button = new JButton(color);
+            button.addActionListener(e -> {
+                myLabel.setText("Selection Process Complete");
+                showDescription(car);
+            });
+            return button;
+        }
 
-            DescriptionFrame df = new DescriptionFrame(text);
-            df.addWindowListener(new WindowAdapter() {
+        private void showDescription(Automobile auto) 
+        {
+            DescriptionFrame df = new DescriptionFrame(auto.getCustomDescription());
+            df.addWindowListener(new WindowAdapter() 
+            {
                 @Override
-                public void windowClosed(WindowEvent e)
+                public void windowClosed(WindowEvent e) 
                 {
                     myLabel.setText("Please Select A File");
                 }
             });
+            df.setVisible(true);
         }
     }
 
-    public static class OtherFrame extends JFrame
+    public static class OtherFrame extends JFrame 
     {
         protected DefaultListModel<Automobile> listModel = new DefaultListModel<>();
         protected JList<Automobile> listView = new JList<>(listModel);
 
-        public OtherFrame (String title)
+        public OtherFrame(String title) 
         {
             super(title);
-            setLocation(100,100);
-            setMinimumSize(new Dimension(300,200));
+            setLocation(100, 100);
+            setMinimumSize(new Dimension(300, 200));
             setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
             JMenuBar menuBar = new JMenuBar();
@@ -185,28 +189,42 @@ public class Experimentation
             menuBar.add(fileMenu);
 
             JMenuItem openItem = new JMenuItem("Open");
-            openItem.setAccelerator(KeyStroke.getKeyStroke("crtl O"));
-            fileOpenMenuItem.addActionListener(e -> openFile());
+            openItem.setAccelerator(KeyStroke.getKeyStroke("ctrl O"));
+            openItem.addActionListener(e -> openFile());
 
             JMenuItem exitItem = new JMenuItem("Exit");
             exitItem.setAccelerator(KeyStroke.getKeyStroke("alt F4"));
-            fileExitMenuItem.addActionListener(e -> dispose());
+            exitItem.addActionListener(e -> dispose());
+
+            fileMenu.add(openItem);
+            fileMenu.add(exitItem);
 
             add(new JScrollPane(listView), BorderLayout.CENTER);
 
             pack();
             setVisible(true);
         }
+
+        private void openFile() 
+        {
+            JFileChooser chooser = new JFileChooser();
+            int result = chooser.showOpenDialog(this);
+            if (result == JFileChooser.APPROVE_OPTION) 
+            {
+                JOptionPane.showMessageDialog(this, 
+                    "Selected: " + chooser.getSelectedFile().getAbsolutePath());
+            }
+        }
     }
 
-    public static class Automobile
+    public static class Automobile 
     {
         protected int year;
         protected String make, model, color, vin;
 
         public Automobile() {}
 
-        public Automobile(int year, String make, String model, String color, String vin)
+        public Automobile(int year, String make, String model, String color, String vin) 
         {
             this.year = year;
             this.make = make;
@@ -215,9 +233,15 @@ public class Experimentation
             this.vin = vin;
         }
 
-        public String getCustomDescription()
+        public String getCustomDescription() 
         {
             return make + " " + model + " (" + year + "), Color: " + color + ", VIN: " + vin;
+        }
+
+        @Override
+        public String toString() 
+        {
+            return getCustomDescription();
         }
     }
 }
